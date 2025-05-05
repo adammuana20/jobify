561e5b3263b0cf77ce665042a026dfad7a03eb85
Adam Muana <adammuanawork@gmail.com>
2025-05-05 17:06:41 +0800

    fix-front-end

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ cloudinary.config({
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -49,7 +49,7 @@ app.use("/api/v1/users", authenticateUser, userRoutes);
 app.use("/api/v1/auth", authRoutes);
 
 app.get("*", (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res, next) => {
