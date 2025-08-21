fdc16c5811a570e12ba297b80529ccc983bd65e3
Adam Muana <adammuanawork@gmail.com>
2025-08-21 14:11:26 +0800

    I have removed the RQDevTool in production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -120,7 +120,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 };
